17a075119c53def7459bed08d1107a86ba67860c
陶浩南 <taoaaron5@gmail.com>
2025-05-11 20:42:17 +1000

    update
    # Conflicts:
    #	.next/cache/webpack/client-development/1.pack.gz
    #	.next/cache/webpack/client-development/2.pack.gz
    #	.next/cache/webpack/client-development/3.pack.gz
    #	.next/cache/webpack/client-development/index.pack.gz
    #	.next/cache/webpack/client-development/index.pack.gz.old
    #	.next/cache/webpack/server-development/0.pack.gz
    #	.next/cache/webpack/server-development/1.pack.gz
    #	.next/cache/webpack/server-development/2.pack.gz
    #	.next/cache/webpack/server-development/3.pack.gz
    #	.next/cache/webpack/server-development/4.pack.gz
    #	.next/cache/webpack/server-development/5.pack.gz
    #	.next/cache/webpack/server-development/index.pack.gz
    #	.next/cache/webpack/server-development/index.pack.gz.old
    #	.next/server/app/page.js
    #	.next/server/server-reference-manifest.js
    #	.next/server/server-reference-manifest.json
    #	.next/server/webpack-runtime.js
    #	.next/static/chunks/app/layout.js
    #	.next/static/chunks/app/page.js
    #	.next/static/chunks/webpack.js
    #	.next/trace
    #	src/app/Data/AchievementData.js
    #	src/app/Data/ProjectsData.js
    #	src/app/components/HeroSection.jsx

--- src/app/Data/ProjectsData.js
+++ src/app/Data/ProjectsData.js
@@ -1,24 +1,60 @@
 const ProjectsData = [
+<<<<<<< HEAD
   {
     id: 0,
     title: "Money Recorder",
     description: "The personal accounting app for recording money",
     tag: ["All", "React", "React Natived"],
     imgUrl: "/images/Projects/Money.png",
+=======
+  // {
+  //   id: 0,
+  //   title: "Personal Blog",
+  //   description: "Personal Responsive Portfolio Web",
+  //   tag: ["All", "React", "MongoDB"],
+  //   imgUrl: "/images/Projects/Blog.png",
+  //   gitUrl: "https://github.com/HAONANTAO/BlogWeb",
+  //   preview: "https://www.aaronblog.top/",
+  // },
+
+  {
+    id: 0,
+    title: "The E-Commerce Website",
+    description: "Gneral e-commerce website show",
+    tag: ["All", "React", "MongoDB"],
+    imgUrl: "/images/Projects/ECommerce.png",
+    gitUrl: "https://github.com/HAONANTAO/E-Commerce-Rabbit",
+    preview: "https://www.wanyancanrui.com/",
+  },
+  {
+    id: 1,
+    title: "Money Recorder",
+    description: "The React Native IOS app",
+    tag: ["All", "ReactNative", "MongoDB"],
+    imgUrl: "/images/Projects/MoneyRecorder.jpg",
+>>>>>>> 185ca0c (update)
     gitUrl: "https://github.com/HAONANTAO/Money_Recorder",
     preview: "https://apps.apple.com/us/app/moneyrecorder/id6744058988",
   },
   {
+<<<<<<< HEAD
     id: 1,
+=======
+    id: 2,
+>>>>>>> 185ca0c (update)
     title: "Mock AI ChatBot",
     description: "The Responsive Mock GPT ChatBot",
     tag: ["All", "React", "TypeScript"],
     imgUrl: "/images/Projects/MockAIChatBot.png",
     gitUrl: "https://github.com/HAONANTAO/Mock_AI_ChatBot",
-    preview: "https://www.wanyancanrui.com/",
+    preview: "https://mock-ai-chat-bot.vercel.app/",
   },
   {
+<<<<<<< HEAD
     id: 2,
+=======
+    id: 3,
+>>>>>>> 185ca0c (update)
     title: "Mock Threads ",
     description: "The Mock Threads website",
     tag: ["All", "NextJS", "MongoDB"],
@@ -27,7 +63,11 @@ const ProjectsData = [
     preview: "http://www.taohaonan.com/",
   },
   {
+<<<<<<< HEAD
     id: 3,
+=======
+    id: 4,
+>>>>>>> 185ca0c (update)
     title: "Evently",
     description: "Evently organized events and manage your events.",
     tag: ["All", "NextJS", "TypeScript"],
@@ -36,7 +76,11 @@ const ProjectsData = [
     preview: "https://evently-three-kohl.vercel.app/",
   },
   {
+<<<<<<< HEAD
     id: 4,
+=======
+    id: 5,
+>>>>>>> 185ca0c (update)
     title: "Game Hub",
     description:
       "GameHub is a video game discovery web app that helps you find new and interesting games to play. ",
@@ -47,6 +91,7 @@ const ProjectsData = [
   },
 
   {
+<<<<<<< HEAD
     id: 5,
     title: "Personal Blog",
     description: "Personal Responsive Portfolio Web",
@@ -56,6 +101,8 @@ const ProjectsData = [
     preview: "https://www.aaronblog.top/",
   },
   {
+=======
+>>>>>>> 185ca0c (update)
     id: 6,
     title: "Issue Tracker",
     description: "The responsive Issue Tracker during development period",
@@ -63,6 +110,15 @@ const ProjectsData = [
     tag: ["All", "NextJS", "MySQL"],
     gitUrl: "https://github.com/HAONANTAO/IssueTracker",
     preview: "/",
+  },
+  {
+    id: 7,
+    title: "TopSocial",
+    description: "Responsive Mock Top Social Media Website",
+    imgUrl: "/images/Projects/TopSocial.png",
+    tag: ["All", "React"],
+    gitUrl: "https://github.com/HAONANTAO/TopSocial",
+    preview: "top-social-q6r851tbu-haonantaos-projects.vercel.app",
   },
 
   // {
@@ -83,6 +139,7 @@ const ProjectsData = [
   //   gitUrl: "https://github.com/HAONANTAO/MERN_Recipe",
   //   preview: "/",
   // },
+<<<<<<< HEAD
   // {
   //   id: 6,
   //   title: "SimpleWebCalculator",
@@ -113,6 +170,29 @@ const ProjectsData = [
     gitUrl: "https://github.com/HAONANTAO/World-Clock",
     preview: "https://iridescent-gaufre-dce53f.netlify.app",
   },
+=======
+  {
+    id: 8,
+    title: "SimpleWebCalculator",
+    description: "Mock SimpleWebCalculator",
+    imgUrl: "/images/Projects/SimpleWebCalculator.png",
+    tag: ["All", "ThreeJS"],
+    gitUrl: "https://github.com/HAONANTAO/SimpleWebCalculator",
+    preview:
+      "https://simple-web-calculator-2lbqba2nx-haonantaos-projects.vercel.app/",
+  },
+
+  // {
+  //   id: 9,
+  //   title: "World Clock",
+  //   description:
+  //     "Using the React + Vite technology stack to create a front-end dynamic page that Showing clocks from four different regions: Melbourne, China, London, and New York.",
+  //   imgUrl: "/images/Projects/WorldClock.png",
+  //   tag: ["All", "React"],
+  //   gitUrl: "https://github.com/HAONANTAO/World-Clock",
+  //   preview: "https://iridescent-gaufre-dce53f.netlify.app",
+  // },
+>>>>>>> 185ca0c (update)
   // {
   //   id: 8,
   //   title: "Personal Static Resume",
@@ -121,7 +201,11 @@ const ProjectsData = [
   //   tag: ["All"],
   //   gitUrl: "https://github.com/HAONANTAO/PersonalResume",
   //   preview: "http://aaron-resume-web.s3-website-us-east-1.amazonaws.com/",
+<<<<<<< HEAD
 
+=======
+  //   // personal-resume-ccib4yq7y-haonantaos-projects.vercel.app
+>>>>>>> 185ca0c (update)
   // },
 ];
 
